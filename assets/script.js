0a6a0bdbda7d373a2f1b66c421fcc8f353f60b47
AMillsy <46033976+AMillsy@users.noreply.github.com>
2023-03-18 17:50:52 +0000

    Using functions and for loops in JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,29 @@
 var topics = [`HTML`, `CSS`, `Git`, `JavaScript`];
+var shapes = ["Rectange", "Triangle", "Square", "Circle"];
 
-for (let i = 0; i < topics.length; i++) {
-  console.log(topics[i]);
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics(arr1) {
+  for (let i = 0; i < arr1.length; i++) {
+    console.log(arr1[i]);
+  }
 }
+
+function selectTopic() {
+  if (randomTopic === "HTML") {
+    console.log("Let's study HTML!");
+  } else if (randomTopic === "CSS") {
+    console.log("Let's study CSS!");
+  } else if (randomTopic === "Git") {
+    console.log("Let's study Git!");
+  } else if (randomTopic === "JavaScript") {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log("Please try again!");
+  }
+}
+
+console.log(`Here are are the topics we learned through Prework:`);
+listTopics(topics);
+console.log(`Which topic should we study first?`);
+selectTopic();
